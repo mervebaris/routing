c0c2217e25e984daaea6e348c4be243815774f48
Merve Barış <mrvbaris1@gmail.com>
2022-09-08 17:19:04 +0300

    init

--- src/pages/DashboardLayout/Contac.js
+++ src/pages/DashboardLayout/Contac.js
@@ -1,11 +1,41 @@
+import { Field, Form, Formik } from 'formik';
 import React from 'react'
 
 function Contac() {
   return (
     <div>
-        <h2>Contac</h2>
+      <h2>Contac</h2>
+
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
-  )
+  );
 }
 
 export default Contac
