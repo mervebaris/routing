9022332da346f38a458965a75f53b4b8d0602fe6
Merve Barış <mrvbaris1@gmail.com>
2022-09-12 12:15:19 +0300

    init

--- src/pages/DashboardLayout/Contac.js
+++ src/pages/DashboardLayout/Contac.js
@@ -1,41 +1,71 @@
-import { Field, Form, Formik } from 'formik';
 import React from 'react'
+import { useFormik } from "formik";
 
 function Contac() {
+  const {handleChange, handleSubmit} = useFormik({
+     initialValues:{
+          firstName: "",
+          lastName: "",
+          email: "",
+          message: "",
+        },
+        onSubmit: async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          console.log(JSON.stringify(values, null, 2));
+        }
+
+  })
+
+
   return (
     <div>
       <h2>Contac</h2>
 
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
+
+      <form onSubmit={handleSubmit} className="form">
+        <div>
           <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
+          <input
+            id="firstName"
+            name="firstName"
+            placeholder="Jane"
+            onChange={handleChange("firstName")}
+          />
+        </div>
 
+        <div>
           <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
+          <input
+            id="lastName"
+            name="lastName"
+            placeholder="Doe"
+            onChange={handleChange("lastName")}
+          />
+        </div>
 
+        <div>
           <label htmlFor="email">Email</label>
-          <Field
+          <input
             id="email"
             name="email"
             placeholder="jane@acme.com"
             type="email"
+            onChange={handleChange("email")}
           />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+        </div>
+        <div>
+          <label htmlFor="email">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your massege... "
+            onChange={handleChange("messsage")}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
 
-export default Contac
+export default Contac;
